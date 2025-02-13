4901f2b69e9e4425c1bdbb2321ebc08f684c9fc6
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2025-02-13 08:45:14 +0100

    ajout des liens dans le composant FooterPage

--- components/FooterPage.vue
+++ components/FooterPage.vue
@@ -6,10 +6,13 @@
                 <li><a href="">CEGEP Vanier</a></li>
                 <li><a href="">Préparer son départ</a></li>
             </ul>
-            <ul class="flex gap-2">
-                <li><a href=""><img src="" alt="iconFacebook"></a></li>
-                <li><a href=""><img src="" alt="iconInsta"></a></li>
-                <li><a href=""><img src="" alt="iconLinked"></a></li>
+            <ul class="flex gap-2 items-center">
+                <li><a href="https://www.facebook.com/mmimontbeliard/?locale=fr_FR"><img
+                            src="../public/icons/icon_facebook.svg" alt="iconFacebook"></a></li>
+                <li><a href="https://www.instagram.com/mmi_montbeliard?igsh=Y2oza3N6bTY0OTBt
+"><img src="../public/icons/icon_instagram.svg" alt="iconInsta"></a></li>
+                <li><a href="https://www.linkedin.com/school/mmimontbeliard"><img
+                            src="../public/icons/icon_linkedin.svg" alt="iconLinked"></a></li>
             </ul>
         </nav>
     </footer>
