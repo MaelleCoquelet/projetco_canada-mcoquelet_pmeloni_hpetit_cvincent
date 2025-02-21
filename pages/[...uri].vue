96e24a4cf6bb353f17578f1d5c16e4b32d038a94
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2025-02-21 13:46:29 +0100

    page 404

--- pages/[...uri].vue
+++ pages/[...uri].vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>URI Page</h1>
+    <div class="flex flex-col items-center justify-center h-screen bg-gray-100 text-center" >
+    <p>Oups ! La page que vous cherchez n'existe pas.</p>
+    <h1 class="text-9xl font-bold text-gray-800 flex items-center">
+        4
+        <img src="/public/img/erable.svg" alt="feuille d'erable" class="h-28 w-28 mx-2" >
+        4
+    </h1>
+    <router-link to="/" class="mt-6 px-4 py-2 bg-red-900 text-white rounded-lg hover:bg-red-800">
+      Retour à l'accueil
+    </router-link>
+</div>
 </template>
