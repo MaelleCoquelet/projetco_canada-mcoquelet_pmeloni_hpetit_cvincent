420062c16a507512bd28f9fb903df28b4b105771
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2025-03-13 10:21:27 +0100

    pages statiques temoigages et evenements

--- pages/evenements.vue
+++ pages/evenements.vue
@@ -1,3 +1,20 @@
 <script setup lang="ts">
 const route = useRoute()
 </script>
+
+<template>
+    <section class="col-span-12 grid grid-cols-12 bg-zinc-600">
+        <nav class="w-full text-white col-start-2 col-end-8">
+            <div class="items-left pt-96 pb-16">
+                <h1 class="text-9xl font-bold ">Événements</h1>
+                <p class="text-2xl" >Nous organisons différents évènements pour vous permettre d’entrer en contact avec d’anciens étudiants déjà partis.</p>
+            </div>
+        </nav>
+    </section>
+    <section class="col-span-12 grid grid-cols-12 text-white bg-red-900">
+        <div class="col-start-3 col-end-11 mt-16 mb-16" >
+            <p class="text-lg mt-5" >La journée portes ouvertes 2025 du département MMI Montbéliard vous invite à découvrir l'univers du multimédia et de la communication numérique. Au programme, des ateliers interactifs, des démonstrations de projets étudiants et des échanges avec nos enseignants et étudiants passionnés. Vous pourrez explorer nos formations innovantes, qui allient création, technique et stratégie digitale, pour préparer les métiers de demain.</p>
+            <p class="text-lg mt-5"  >Cet événement est une occasion unique de plonger dans l'ambiance dynamique de notre département et d'obtenir des réponses à toutes vos questions. Que vous soyez curieux, futur étudiant ou simplement intéressé par le monde du digital, venez vivre une journée riche en découvertes et en inspirations dans un cadre convivial et stimulant.</p>
+        </div>
+    </section>
+</template>
---
+++ pages/temoignages.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+    <section class="col-span-12 grid grid-cols-12 bg-zinc-600">
+        <nav class="w-full text-white col-start-2 col-end-8">
+            <div class="items-left pt-96 pb-16">
+                <h1 class="text-9xl font-bold ">Témoignages</h1>
+                <p class="text-2xl mt-6" >Retrouvez ici, les témoignages d’anciens étudiants ayant déjà réalisé l’échange au Canada.</p>
+            </div>
+        </nav>
+    </section>
+    <section class="col-span-12 grid grid-cols-12 text-white bg-red-900">
+        <div class="col-start-3 col-end-11 mt-16 mb-16" >
+            <p class="text-lg mt-5" >Nous comprenons à quel point il est essentiel, tant pour les étudiants que pour leurs parents, de savoir à quoi s'attendre afin de préparer au mieux cette aventure. C'est pourquoi nous avons recueilli les témoignages de plusieurs anciens participants ayant déjà vécu cet échange avec succès.</p>
+            <p class="text-lg mt-5"  >Grâce à ces récits écrits et vidéos, vous découvrirez les défis auxquels vous pourriez être confrontés et obtiendrez des conseils précieux pour vous y préparer sereinement.</p>
+        </div>
+    </section>
+</template>
