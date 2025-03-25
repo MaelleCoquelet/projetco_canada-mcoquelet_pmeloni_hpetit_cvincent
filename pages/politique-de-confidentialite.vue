a5a0b8c2afef1587d27cb2959eb9d4a9454f924d
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2025-03-25 09:36:29 +0100

    Refactor mentions légales and CGU pages with improved layout and responsive design

--- pages/politique-de-confidentialite.vue
+++ pages/politique-de-confidentialite.vue
@@ -3,16 +3,16 @@
 
 
 <template>
-    <div class="flex flex-col gap-8 p-8">
+    <div class="flex flex-col gap-8 p-8 px-20 py-12">
         <h1 class="text-5xl md:text-6xl">Politique de confidentialité</h1>
-        <p>Bienvenue sur LiftOff MMI, la plateforme d'information pour les échanges scolaire au canada. Chez LiftOff
+        <p class="text-base md:text-lg">Bienvenue sur LiftOff MMI, la plateforme d'information pour les échanges scolaire au canada. Chez LiftOff
             MMI, la protection de vos données personnelles est notre priorité. Cette Politique de Confidentialité vise à
             vous informer sur la manière dont nous collectons, utilisons, protégeons, et traitons vos données
             personnelles. En utilisant nos services, vous acceptez les conditions de cette politique.</p>
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">1. Les Données Personnelles que nous collectons</h2>
             <h3 class="text-lg md:text-2xl">1.1. Données que vous fournissez directement</h3>
-            <p>Contact : Adresse e-mail, Nom, Prénom, contenu de la demande, objet et niveaux d'études.</p>
+            <p class="text-base md:text-lg">Contact : Adresse e-mail, Nom, Prénom, contenu de la demande, objet et niveaux d'études.</p>
             <h3 class="text-lg md:text-2xl">1.2. Données collectées automatiquement</h3>
             <ul>
                 <li>Données de connexion : Adresse IP, type de navigateur, langue, et activité de connexion.</li>
@@ -24,13 +24,13 @@
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">2. Finalités du Traitement</h2>
-            <p>Nous traitons vos données personnelles pour :
+            <p class="text-base md:text-lg">Nous traitons vos données personnelles pour :
                 Fournir une assistance client et répondre aux demandes de support.</p>
         </div>
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">3. Destinataires de vos Données Personnelles</h2>
-            <p>Vos données peuvent être partagées avec :</p>
+            <p class="text-base md:text-lg">Vos données peuvent être partagées avec :</p>
             <ul>
                 <li>Nos sous-traitants sont chargés de l'hébergement et de la maintenance technique.</li>
                 <li>Les autorités administratives ou judiciaires en cas de nécessité légale.</li>
@@ -39,7 +39,7 @@
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">4. Sécurité des Données Personnelles</h2>
-            <p>Nous mettons en œuvre des mesures de sécurité pour protéger vos données, stockées sur des serveurs
+            <p class="text-base md:text-lg">Nous mettons en œuvre des mesures de sécurité pour protéger vos données, stockées sur des serveurs
                 sécurisés.
                 Les informations sensibles, comme les identifiants de connexion, sont cryptées pour garantir leur
                 confidentialité.</p>
@@ -47,14 +47,14 @@
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">5. Durée de Conservation</h2>
-            <p>Vos données sont conservées pendant toute la durée du tournoi et jusqu’à 1 an après la fin de
+            <p class="text-base md:text-lg">Vos données sont conservées pendant toute la durée du tournoi et jusqu’à 1 an après la fin de
                 l’événement,
                 sauf en cas de demande spécifique de suppression.</p>
         </div>
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">6. Vos Droits</h2>
-            <p>Vous avez le droit d'accéder, rectifier, ou demander l'effacement de vos données. Vous pouvez également
+            <p class="text-base md:text-lg">Vous avez le droit d'accéder, rectifier, ou demander l'effacement de vos données. Vous pouvez également
                 vous
                 opposer au traitement et exercer votre droit à la portabilité. Pour exercer ces droits, contactez-nous
                 sur
@@ -63,23 +63,23 @@
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">7. Vie Privée des Enfants</h2>
-            <p>Nos services ne sont pas destinés aux enfants de moins de 13 ans. Si vous êtes parent et que vous avez
+            <p class="text-base md:text-lg">Nos services ne sont pas destinés aux enfants de moins de 13 ans. Si vous êtes parent et que vous avez
                 connaissance que votre enfant nous a fourni des données personnelles, contactez-nous pour les supprimer.
             </p>
         </div>
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">8. Modifications de la Politique de Confidentialité</h2>
-            <p>Nous nous réservons le droit de modifier cette politique à tout moment. Les modifications prendront effet
+            <p class="text-base md:text-lg">Nous nous réservons le droit de modifier cette politique à tout moment. Les modifications prendront effet
                 dès
                 leur publication sur le site.</p>
         </div>
 
         <div class="flex flex-col gap-2">
             <h2 class="text-xl md:text-3xl">9. Contact</h2>
-            <p>Pour toutes questions ou demandes concernant cette politique, veuillez nous contacter directement sur le
+            <p class="text-base md:text-lg">Pour toutes questions ou demandes concernant cette politique, veuillez nous contacter directement sur le
                 formulaire de contact.</p>
-            <p>Merci d’utiliser LiftOff MMI pour se lancer dans une nouvelle expérience.</p>
+            <p class="text-base md:text-lg">Merci d’utiliser LiftOff MMI pour se lancer dans une nouvelle expérience.</p>
         </div>
     </div>
 </template>
