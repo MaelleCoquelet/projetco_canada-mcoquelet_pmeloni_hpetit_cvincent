e31be01c55bdd4e31c80ab5c391a9e42ec88ec5e
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2025-03-25 08:47:14 +0100

    Amélioration de la mise en page de la page Matane avec des ajustements de styles et ajout de responsive

--- pages/matane.vue
+++ pages/matane.vue
@@ -3,8 +3,8 @@ const route = useRoute()
 </script>
 
 <template>
-    <section class="col-span-12 grid grid-cols-12 bg-[url(/img/matane.jpg)]">
-        <nav class="w-full text-white col-start-2 col-end-8">
+    <section class="md:col-span-12 md:grid md:grid-cols-12 bg-[url(/img/matane.jpg)] p-8">
+        <nav class="w-full text-white md:col-start-2 md:col-end-8">
             <div class="items-left pt-96 pb-16">
                 <h1 class="text-6xl md:text-9xl font-bold ">MATANE</h1>
                 <h2 class="text-3xl md:text-6xl font-bold">CEGEP</h2>
@@ -12,9 +12,9 @@ const route = useRoute()
             </div>
         </nav>
     </section>
-    <section class="col-span-12 grid grid-cols-12 text-white bg-red-900">
-        <div class="col-start-3 col-end-11 mt-16 mb-16">
-            <h4 class="text-3xl font-semibold">Qu'est-ce que le Cegep de Matane ?</h4>
+    <section class="md:col-span-12 md:grid md:grid-cols-12 text-white bg-red-900 p-8">
+        <div class="flex flex-col gap-6 md:col-start-3 md:col-end-11 md:mt-16 md:mb-16">
+            <h4 class="text-2xl md:text-3xl font-semibold">Qu'est-ce que le Cegep de Matane ?</h4>
             <p class="text-lg mt-5">Le Cégep de Matane est l'un des 48 cégeps au Québec, fondé en 1967 dans la ville de
                 Matane, située dans la région de la Gaspésie. Il est reconnu pour son environnement chaleureux, ses
                 infrastructures modernes et son engagement envers la réussite éducative. Son histoire reflète l’objectif
@@ -27,8 +27,8 @@ const route = useRoute()
                 l'intégration des nouvelles technologies et des outils numériques dans ses formations.</p>
 
 
-            <h4 class="text-3xl font-semibold mt-10">Pourquoi on aime tant Matane ?</h4>
-            <div class=" flex p-6 bg-white/10 items-center rounded-lg mt-5">
+            <h4 class="text-2xl md:text-3xl font-semibold">Pourquoi on aime tant Matane ?</h4>
+            <div class="flex gap-2 p-6 bg-white/10 items-center rounded-lg">
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
                     <g fill="none">
@@ -39,30 +39,33 @@ const route = useRoute()
                     </g>
                 </svg>
 
-                <p class=" text-xl ml-9 font-semibold text-left">Un cadre naturel exceptionnel – Paysages québécois
+                <p class="text-base md:text-xl font-semibold text-left">Un cadre naturel exceptionnel – Paysages
+                    québécois
                     accessibles pour profiter de la nature.</p>
             </div>
 
 
-            <div class=" flex p-6 bg-white/10 items-center rounded-lg mt-5">
+            <div class="flex gap-2 p-6 bg-white/10 items-center rounded-lg">
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
                     <path fill="currentColor"
                         d="M12.394 16.958c0-.789.338-.902 1.127-.451a54 54 0 0 0 2.704 1.465c0-.45.451-.789 1.24-.564s1.577.338 1.577.338s-.45-1.014-.676-1.464c-.338-.789 0-1.24.338-1.352c0 0-.45-.338-.789-.564c-.676-.45-.563-1.014.113-1.24c.902-.45 2.141-.9 2.141-.9c-.338-.226-.789-.79-.338-1.578c.45-.676 1.24-1.69 1.24-1.69H18.93c-.79 0-1.015-.676-1.015-1.127c0 0-1.239.901-2.14 1.465c-.79.563-1.465 0-1.352-.902a37 37 0 0 0 .338-2.93c-.451.451-1.24.339-1.69-.337c-.564-1.127-1.127-2.48-1.127-2.48S11.38 4 10.817 5.128c-.338.676-1.127.788-1.578.45a37 37 0 0 0 .338 2.93c.113.789-.563 1.352-1.352.789c-.901-.564-2.253-1.465-2.253-1.465c0 .45-.226 1.014-1.014 1.127H2.817s.789 1.014 1.24 1.69s0 1.352-.339 1.577c0 0 1.127.564 2.141.902c.676.338.902.788.113 1.24c-.226.225-.789.563-.789.563c.45.112.789.563.45 1.352c-.225.45-.675 1.464-.675 1.464s.788-.225 1.577-.338c.789-.225 1.127.226 1.24.564c0 0 1.352-.789 2.704-1.465c.676-.45 1.127-.225 1.127.45v1.916c0 1.127-.226 2.254-.564 2.93c-5.07-.564-9.352-4.62-9.352-10.028c0-5.521 4.62-10.029 10.366-10.029c5.747 0 10.367 4.508 10.367 10.029c0 5.183-4.057 9.464-9.24 10.028v1.352C19.268 22.592 24 17.746 24 11.775C24 5.352 18.592.282 11.944.282C5.408.282 0 5.352 0 11.662c0 5.521 4.169 10.14 9.69 11.155c.902.225 1.465.338 2.028.901c.564-1.126.676-3.38.676-4.62Z" />
                 </svg>
 
-                <p class=" text-xl ml-9 font-semibold text-left">Vie Québécoise authentique – Matane offre une immersion
+                <p class="text-base md:text-xl font-semibold text-left">Vie Québécoise authentique – Matane offre une
+                    immersion
                     dans la culture du Québec. </p>
             </div>
 
 
-            <div class=" flex p-6 bg-white/10 items-center rounded-lg mt-5">
+            <div class="flex gap-2 p-6 bg-white/10 items-center rounded-lg">
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
                     <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="1.5" d="M16 7a2 2 0 1 0 0-4a2 2 0 0 0 0 4m0 0v10c0 6-10 6-10 0v-4l2 2" />
                 </svg>
-                <p class=" text-xl ml-9 font-semibold text-left">Activités locales – Feux de camp, pêche au saumon,
+                <p class="text-base md:text-xl font-semibold text-left">Activités locales – Feux de camp, pêche au
+                    saumon,
                     dégustation de poutine. </p>
             </div>
 
@@ -71,37 +74,39 @@ const route = useRoute()
 
     <!-- Temoignages -->
 
-    <section class="col-span-12 grid grid-cols-12 text-black bg-red-100">
-        <div class="col-start-3 col-end-11 mt-16 mb-16">
+    <section class="md:col-span-12 md:grid md:grid-cols-12 text-black bg-red-100 p-8">
+        <div class="md:col-start-3 md:col-end-11 md:mt-16 md:mb-16">
             <h5 class="text-2xl font-bold mb-8">Qu’en disent les étudiants ?</h5>
             <div class="relative">
 
 
-                <p class="text-9xl absolute top-0 left-0 text-red-900 opacity-25">“</p>
-                <p class="ml-16 text-lg">Que ce soit sur le plan académique ou personnel, il faut se rappeler que c’est
+                <p class="text-9xl absolute top-0 -left-5 text-red-900 opacity-25">“</p>
+                <p class="ml-10 md:ml-16 text-lg">Que ce soit sur le plan académique ou personnel, il faut se rappeler que
+                    c’est
                     une expérience unique que nous vivrons peut-être une seule fois. Il est important d’en profiter au
                     maximum, de saisir toutes les occasions qui se présentent pour découvrir de nouvelles choses et
                     rencontrer des gens. Les activités d’intégration organisées par le cégep sont le meilleur moyen de
                     se faire des connaissances.”</p>
-                <p class="ml-16 font-bold text-right">Chloé VINCENT</p>
-                <p class="ml-16 text-right">échange de août à décembre 2024</p>
+                <p class="ml-10 md:ml-16 font-bold text-right">Chloé VINCENT</p>
+                <p class="ml-10 md:ml-16 text-right">échange de août à décembre 2024</p>
             </div>
 
 
             <div class="relative mt-10">
-                <p class="text-9xl absolute top-0 left-0 text-red-900 opacity-25">“</p>
-                <p class="ml-16 text-lg">La générosité et l’accueil des québécois était particulièrement appréciable. De
+                <p class="text-9xl absolute top-0 -left-5 text-red-900 opacity-25">“</p>
+                <p class="ml-10 md:ml-16 text-lg">La générosité et l’accueil des québécois était particulièrement appréciable.
+                    De
                     même, le fait que tous les québécois nous disent qu’une poutine se mange “au bon moment” était assez
                     drôle et marquant.”</p>
-                <p class="ml-16 font-bold text-right">Maëlle COQUELET</p>
-                <p class="ml-16 text-right">échange de août à décembre 2024</p>
+                <p class="ml-10 md:ml-16 font-bold text-right">Maëlle COQUELET</p>
+                <p class="ml-10 md:ml-16 text-right">échange de août à décembre 2024</p>
             </div>
         </div>
 
     </section>
-    <section class="bg-red-100 flex justify-center">
+    <section class="bg-red-100 flex justify-center p-8">
         <a href="/temoignages"
-            class="text-white flex bg-red-900 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Voir
+            class="text-white flex bg-red-900 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-base md:text-lg px-6 py-3 text-center me-2 mb-2">Voir
             plus de témoignages</a>
     </section>
 </template>
